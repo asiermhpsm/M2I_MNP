de2b570a92f8e35151fe3b57efdf7f8528b3a21f
Asier <asiermhpsm@gmail.com>
2024-12-05 00:36:51 +0100

    Hecho para 2x2

--- main.f90
+++ main.f90
@@ -2,75 +2,55 @@ program main
     use iso_Fortran_env, only: real64
     implicit none
 
-    real(real64), allocatable :: A(:,:), b(:,:), H(:,:), Hamp(:,:)
+    real(real64), allocatable :: A(:,:), b(:,:), x(:)
     integer :: i, j
     
     A = reshape(real([1,2,3,4,5,6,7,8,9], real64), [3,3], order=[2,1])
-    b = reshape(real([5,6], real64), [2,1])
-    call QR(A)
+    b = reshape(real([10,11,12], real64), [3,1])
+    call QR(A,b)
 
+    call solSist(A,b,x)
 
+    call compruebaSol(reshape(real([1,2,3,4], real64), [2,2], order=[2,1]), x, reshape(real([5,6], real64), [2,1]))
 
     contains
-        subroutine QR(A)
-            ! Resuleve el sistema Ax = b usando la factorización QR
+        subroutine QR(A, b)
+            ! Transforma la matriz A y el vector b para resolver un sistema triangular superior usando QR
             ! A: matriz de coeficientes
-            real(real64), allocatable :: A(:,:), v(:), H(:,:), Hamp(:,:)
+            ! b: vector (columna, i.e matriz nx1) de términos independientes
+            real(real64) :: A(:,:), b(:,:), Hamp(size(A,1),size(A,2))
+            real(real64), allocatable :: v(:), H(:,:)
             integer :: k
+
+            if (size(A,1) /= size(A,2) .or. size(A,1) /= size(b,1) .or. size(b,2) /= 1) then
+                ERROR STOP "El sistema no está bien planteado ya que las dimensiones de los coeficientes no coinciden"
+            end if
 
             do k = 1, size(A,2)-1
                 ! Calcula el vector vk = ak - ||ak||e1
                 v = A(k:,k)
                 v(1) = v(1) - norm2(v)
 
-                ! Calcula la matriz de Householder H
-                call Householder(v, H)
-                allocate(Hamp(size(A,1),size(A,1)))
+                ! Calcula la matriz de Householder (ampliada) Hamp
+                H = -2/dot_product(v, v)*matmul(reshape(v, [size(v), 1]), reshape(v, [1, size(v)]))
+                do i=1,size(H,1)
+                    H(i,i) = 1 + H(i,i)
+                end do
                 call HouseholderAmpliada(H, size(A,1), Hamp)
                 
 
-                ! Actualiza la matriz A
+                ! Actualiza las matrices A y b
                 A = matmul(Hamp, A)
-                print *, "A:-----------------"
-                do i=1, size(A,1)
-                    do j=1, size(A,2)
-                        print *, i, j, A(i,j)
-                    end do
-                end do
+                b = matmul(Hamp, b)
 
-                deallocate(Hamp)
             end do
 
         end subroutine QR
 
-        subroutine Householder(v, H)
-            ! Calcula la matriz de Householder H = I - 2/(vT*v) * v*vT
-            ! v: vector sobre el que se calcula la matriz
-            ! H: matriz de Householder
-            real(real64), intent(in), allocatable :: v(:)
-            real(real64), intent(inout), allocatable :: H(:,:)
-            real(real64), allocatable :: aux
-            integer :: i,j
-
-            H = matmul(reshape(v, [size(v), 1]), reshape(v, [1, size(v)]))
-            aux = 2/dot_product(v, v)
-            do i=1, size(H,1)
-                do j=1, size(H,2)
-                    if (i == j) then
-                        H(i,j) = 1 - aux*H(i,j)
-                    else
-                        H(i,j) = -aux*H(i,j)
-                    end if
-                end do
-            end do
-            
-        end subroutine Householder
-
-
         subroutine HouseholderAmpliada(H, n, Hamp)
             real(real64), intent(in), allocatable :: H(:,:)
             integer, intent(in) :: n
-            real(real64), intent(inout), allocatable :: Hamp(:,:)
+            real(real64), intent(out) :: Hamp(:,:)
             integer :: i, j, dif
 
             dif = n - size(H,1)
@@ -90,5 +70,46 @@ program main
             
         end subroutine HouseholderAmpliada
         
+        subroutine solSist(A, b, x)
+            ! Resuelve un sistema triangular superior (cuadrado)
+            ! A: matriz de coeficientes
+            ! b: vector (columna, i.e matriz nx1) de términos independientes
+            ! x: vector solucion
+            real(real64), allocatable :: A(:,:), b(:,:), x(:)
+            real(real64) :: aux
+            integer :: i, j, n
+
+            if (size(A,1) /= size(A,2) .or. size(A,1) /= size(b,1) .or. size(b,2) /= 1) then
+                ERROR STOP "El sistema triangular a resolver no está bien planteado ya que las dimensiones no coinciden"
+            end if
+
+            n = size(A,1)
+            print *, "X:"
+            allocate(x(n))
+            x(n) = b(n,1)/A(n,n)
+            print *, n, x(n)
+            do i=n-1,1,-1
+                aux = 0
+                do j=i+1,n
+                    aux = aux + A(i,j)*x(j)
+                end do
+                x(i) = (b(i,1)-aux)/A(i,i)
+                print *, i, x(i)
+            end do
+            
+        end subroutine solSist
+
+        subroutine compruebaSol(A, x, b)
+            real(real64) :: A(:,:), b(:,:), x(:)
+            real(real64), allocatable :: aux(:,:)
+            aux = matmul(A,reshape(x, [2,1])) - b
+            print *, "A*x-b:"
+            do i=1, size(aux,1)
+                do j=1, size(aux,2)
+                    print *, i, j, aux(i,j)
+                end do
+            end do
+            
+        end subroutine compruebaSol
 
 end program main
